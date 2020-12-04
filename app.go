5b57135cf83193c74af45b9c43584bfe0c96bd4f
manlan <2733284198@qq.com>
2020-12-05 03:43:47 +0800

    2020-12-5 03:33:47 1、iris

--- app.go
+++ app.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/kataras/iris/v12"
 )
 
 func main() {
 	app := iris.New()
 
-	app.Get("/hello", func(context iris.Context) {
-		_, _ = context.WriteString("hello")
-		_, _ = context.WriteString(" world")
+	app.Get("/hello/:id", func(context iris.Context) {
+
+		//_, _ = context.WriteString("hello")
+		//_, _ = context.WriteString(" world")
+
+		context.WriteString("hello user" + context.Params().Get("id"))
+		fmt.Println(context.Params().Get("id"))
+
+		context.JSON(iris.Map{
+			"name": " hello  iris",
+		})
 	})
 
 	_ = app.Listen(":880")
